83f6d8149cdab7fc8e0b462509e90724839742ba
MaximeSemblat <166526438+MaximeSemblat@users.noreply.github.com>
2024-04-10 22:31:06 +0100

    Add files via upload

---
+++ DeepGP_four_layers.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import gpflux
+import gpflow
+import tensorflow as tf
+import numpy as np
+
+class dgp_three_layers:
+    def __init__(self, x):
+        
+        X=x
+        # Layer 1
+        Z = np.linspace(X.min(), X.max(), X.shape[0] // 2).reshape(-1, 2)
+        kernel1 = gpflow.kernels.SquaredExponential()
+        inducing_variable1 = gpflow.inducing_variables.InducingPoints(Z.copy())
+        gp_layer1 = gpflux.layers.GPLayer(
+           kernel1, inducing_variable1, num_data=X.shape[0],num_latent_gps=2)
+
+        # Layer 2
+        kernel2 = gpflow.kernels.SquaredExponential()
+        inducing_variable2 = gpflow.inducing_variables.InducingPoints(Z.copy())
+        gp_layer2 = gpflux.layers.GPLayer(
+           kernel2,
+           inducing_variable2,
+           num_data=X.shape[0],
+           num_latent_gps=2,
+        )
+        # Layer 3
+        kernel3 = gpflow.kernels.SquaredExponential()
+        inducing_variable3 = gpflow.inducing_variables.InducingPoints(Z.copy())
+        gp_layer3= gpflux.layers.GPLayer(
+           kernel3,
+           inducing_variable3,
+           num_data=X.shape[0],
+           num_latent_gps=2,
+        )
+        # layer 4
+        kernel4 = gpflow.kernels.RationalQuadratic()
+        inducing_variable4 = gpflow.inducing_variables.InducingPoints(Z.copy())
+        gp_layer4= gpflux.layers.GPLayer(
+           kernel4,
+           inducing_variable4,
+           num_data=X.shape[0],
+           num_latent_gps=12,
+           mean_function=gpflow.mean_functions.Zero(),
+        )
+        # Initialise likelihood and build model
+        likelihood_layer_4 = gpflux.layers.LikelihoodLayer(gpflow.likelihoods.Gaussian(0.1))
+        self.four_layer_dgp = gpflux.models.DeepGP([gp_layer1, gp_layer2,gp_layer3,gp_layer4], likelihood_layer_4)
+        
+    def fit(self, x, y):
+        X= x
+        Y= y
+        # Compile and fit
+        model = self.four_layer_dgp.as_training_model()
+        model.compile(tf.optimizers.Adam(0.01))
+        history_4 = model.fit({"inputs": X, "targets": Y}, epochs=int(1e3), verbose=0)
+        
+        return 'model fit and ready for prediction'
+    
+    def mean_predict(self,x):
+        self.prediction_model = self.four_layer_dgp.as_prediction_model()
+        self.mean = self.prediction_model(x).y_mean.numpy()
+        
+        return (self.mean) 
+        
+
---
+++ DeepGP_two_layers.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import gpflux
+import gpflow
+import tensorflow as tf
+import numpy as np 
+        
+class dgp_two_layers:
+    def __init__(self, x):
+        
+        X=x
+        # Layer 1
+        Z = np.linspace(X.min(), X.max(), X.shape[0] // 2).reshape(-1, 2)
+        kernel1 = gpflow.kernels.SquaredExponential()
+        inducing_variable1 = gpflow.inducing_variables.InducingPoints(Z.copy())
+        gp_layer1 = gpflux.layers.GPLayer(
+            kernel1, inducing_variable1, num_data=X.shape[0],num_latent_gps=2)
+
+        # Layer 2
+        kernel2 = gpflow.kernels.RationalQuadratic()
+        inducing_variable2 = gpflow.inducing_variables.InducingPoints(Z.copy())
+        gp_layer2 = gpflux.layers.GPLayer(
+           kernel2,
+           inducing_variable2,
+           num_data=X.shape[0],
+           num_latent_gps=12,
+           mean_function=gpflow.mean_functions.Zero(),
+        )
+
+        # Initialise likelihood and build model
+        likelihood_layer = gpflux.layers.LikelihoodLayer(gpflow.likelihoods.Gaussian(0.1))
+        self.two_layer_dgp = gpflux.models.DeepGP([gp_layer1, gp_layer2], likelihood_layer)
+    def fit(self, x, y):
+        X= x
+        Y= y
+        
+        # Compile and fit
+        model = self.two_layer_dgp.as_training_model()
+        model.compile(tf.optimizers.Adam(0.01))
+        history = model.fit({"inputs": X, "targets": Y}, epochs=int(1e2), verbose=0)
+        
+        return 'model fit and ready for prediction'
+    
+        mean_prediction_dgpc= model.predict({"inputs": self.x_test_, "targets": np.zeros_like(self.y_test_)})
+        self.loss_two= mse_loss(torch.from_numpy(mean_prediction_dgpc),torch.from_numpy(self.y_test_))
+        self.ui.label_loss.setText("the MSE loss is: " + str(self.loss_two.item()))
+        
+    def mean_predict(self,x):
+        self.prediction_model = self.two_layer_dgp.as_prediction_model()
+        self.mean = self.prediction_model(x).y_mean.numpy()
+        
+        return (self.mean) 
+
